9f26b3271d85c3b0baece527bea1da7c576f4dec
Tiago Almeida <tyagow@hotmail.com.br>
2018-07-08 17:04:09 -0300

    feat: user can submit and save data

--- services/client/src/components/__tests__/UserList.test.jsx
+++ services/client/src/components/__tests__/UserList.test.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { shallow } from "enzyme";
+import renderer from "react-test-renderer";
 
 import UsersList from "../UsersList";
 
@@ -23,4 +24,9 @@ test("UsersList renders properly", () => {
   const element = wrapper.find("h4");
   expect(element.length).toBe(2);
   expect(element.get(0).props.children).toBe("michael");
+});
+
+test("UsersList renders a snapshot properly", () => {
+  const tree = renderer.create(<UsersList users={users} />).toJSON();
+  expect(tree).toMatchSnapshot();
 });
