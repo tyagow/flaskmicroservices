6d1356cefc08ddaa667d47c1a698a7620640998f
Tiago Almeida <tyagow@hotmail.com.br>
2018-07-11 19:40:01 -0300

    feat: adding migrations and tests to User

--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -7,11 +7,13 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 from flask_debugtoolbar import DebugToolbarExtension
+from flask_migrate import Migrate
 
 
 # instantiate the extensions
 db = SQLAlchemy()
 toolbar = DebugToolbarExtension()
+migrate = Migrate()
 
 
 def create_app(script_info=None):
@@ -29,6 +31,7 @@ def create_app(script_info=None):
     # set up extensions
     db.init_app(app)
     toolbar.init_app(app)
+    migrate.init_app(app, db)
 
     # register blueprints
     from project.api.users import users_blueprint
