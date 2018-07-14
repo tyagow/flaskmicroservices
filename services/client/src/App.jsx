75098ee7d9db01d4260271531757816af2e1345d
Tiago Almeida <tyagow@hotmail.com.br>
2018-07-14 11:26:45 -0300

    (client: feat): new routes and form added
    - App adding routes to about login and register
    - Userlist adding new style class well
    - Form and Navbar - Two new components that render a navbar and a generic form used in both login and register routes

--- services/client/src/App.jsx
+++ services/client/src/App.jsx
@@ -4,6 +4,8 @@ import { Route, Switch } from "react-router-dom";
 import UsersList from "./components/UsersList";
 import AddUser from "./components/AddUser";
 import About from "./components/About";
+import NavBar from "./components/NavBar";
+import Form from "./components/Form";
 
 class App extends Component {
   constructor() {
@@ -11,7 +13,13 @@ class App extends Component {
     this.state = {
       users: [],
       username: "",
-      email: ""
+      email: "",
+      title: "TestDriven.io",
+      formData: {
+        username: "",
+        email: "",
+        password: ""
+      }
     };
     this.addUser = this.addUser.bind(this);
     this.handleChange = this.handleChange.bind(this);
@@ -52,32 +60,45 @@ class App extends Component {
   }
   render() {
     return (
-      <div className="container">
-        <div className="row">
-          <div className="col-md-6">
-            <br />
-            <Switch>
-              <Route
-                exact
-                path="/"
-                render={() => (
-                  <div>
-                    <h1>All Users</h1>
-                    <hr />
-                    <br />
-                    <AddUser
-                      username={this.state.username}
-                      email={this.state.email}
-                      handleChange={this.handleChange}
-                      addUser={this.addUser}
-                    />
-                    <br />
-                    <UsersList users={this.state.users} />
-                  </div>
-                )}
-              />
-              <Route exact path="/about" component={About} />
-            </Switch>
+      <div>
+        <NavBar title={this.state.title} />
+        <div className="container">
+          <div className="row">
+            <div className="col-md-6">
+              <br />
+              <Switch>
+                <Route
+                  exact
+                  path="/"
+                  render={() => (
+                    <div>
+                      <h1>All Users</h1>
+                      <hr />
+                      <br />
+                      <AddUser
+                        username={this.state.username}
+                        email={this.state.email}
+                        handleChange={this.handleChange}
+                        addUser={this.addUser}
+                      />
+                      <br />
+                      <UsersList users={this.state.users} />
+                    </div>
+                  )}
+                />
+                <Route exact path="/about" component={About} />
+                <Route
+                  exact
+                  path="/register"
+                  render={() => <Form formType={"Register"} formData={this.state.formData} />}
+                />
+                <Route
+                  exact
+                  path="/login"
+                  render={() => <Form formType={"Login"} formData={this.state.formData} />}
+                />
+              </Switch>
+            </div>
           </div>
         </div>
       </div>
--- services/client/src/components/UsersList.jsx
+++ services/client/src/components/UsersList.jsx
@@ -1,21 +1,15 @@
-import React from 'react';
-
-const UsersList = (props) => {
+import React from "react";
+const UsersList = props => {
   return (
     <div>
-      {
-        props.users.map((user) => {
-          return (
-            <h4
-              key={user.id}
-              className="card card-body bg-light"
-            >{user.username}
-            </h4>
-          )
-        })
-      }
+      {props.users.map(user => {
+        return (
+          <h4 key={user.id} className="well">
+            {user.username}
+          </h4>
+        );
+      })}
     </div>
-  )
+  );
 };
-
 export default UsersList;
