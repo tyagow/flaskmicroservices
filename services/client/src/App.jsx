be8ee51b19f94dfccf6caac203adf4a43cca4809
Tiago Almeida <tyagow@hotmail.com.br>
2018-07-14 14:44:02 -0300

    (client: feat): UserStatus dots /status route

--- services/client/src/App.jsx
+++ services/client/src/App.jsx
@@ -7,6 +7,7 @@ import About from "./components/About";
 import NavBar from "./components/NavBar";
 import Form from "./components/Form";
 import Logout from "./components/Logout";
+import UserStatus from "./components/UserStatus";
 
 class App extends Component {
   constructor() {
@@ -101,7 +102,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <NavBar title={this.state.title} />
+        <NavBar title={this.state.title} isAuthenticated={this.state.isAuthenticated} />
         <div className="container">
           <div className="row">
             <div className="col-md-6">
@@ -162,6 +163,11 @@ class App extends Component {
                       isAuthenticated={this.state.isAuthenticated}
                     />
                   )}
+                />
+                <Route
+                  exact
+                  path="/status"
+                  render={() => <UserStatus isAuthenticated={this.state.isAuthenticated} />}
                 />
               </Switch>
             </div>
